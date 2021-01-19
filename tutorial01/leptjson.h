ee7bb1d493515a11844d1d8102a891510e830de8
Teemo-Tristana <36654157+Teemo-Tristana@users.noreply.github.com>
2021-01-19 15:26:25 +0800

    recode tutorial

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,11 +1,23 @@
-#ifndef LEPTJSON_H__
-#define LEPTJSON_H__
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+/**
+ * 一般会在头文件中进行 #include ，但是头文件中也可能会有其他头文间，为了避免重复声明，一般用 宏 来进行include防范(include guard)
+ *  宏 的名字必须唯一 ，后缀以 _H__ 作为后缀
+*/
+ 
+#ifndef LEPTJSOH_H__
+#define LEPTJSOH_H__
 
-typedef struct {
+
+//类型枚举: c 语言没有 C++ 的命名空间，因此，一般是使用项目的简写作为标识符的前缀， 通常枚举值用全大写，类型和函数名则用小写
+typedef enum {LEPT_NULL, LEPT_FALSE, LEPT_TURE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT} lept_type;
+
+
+typedef struct 
+{
+    /* data */
     lept_type type;
 }lept_value;
+
 
 enum {
     LEPT_PARSE_OK = 0,
@@ -14,8 +26,10 @@ enum {
     LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
+// 解析 JSON
 int lept_parse(lept_value* v, const char* json);
 
+// 获取类型
 lept_type lept_get_type(const lept_value* v);
 
 #endif /* LEPTJSON_H__ */
--- tutorial01/test.c
+++ tutorial01/test.c
@@ -1,33 +1,52 @@
+/**
+ * 
+ * 一般来说，软件开发是以周期进行的。例如，加入一个功能，再写关于该功能的单元测试。但也有另一种软件开发方法论，称为测试驱动开发（test-driven development, TDD），它的主要循环步骤是：
+ * 1. 加入一个测试。
+ * 2. 运行所有测试，新的测试应该会失败。
+ * 3. 编写实现代码。
+ * 4. 运行所有测试，若有测试失败回到3。
+ * 5. 重构代码。
+ * 回到 1。
+ * TDD 是先写测试，再实现功能。好处是实现只会刚好满足测试，而不会写了一些不需要的代码，或是没有被测试的代码。
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "leptjson.h"
 
 static int main_ret = 0;
 static int test_count = 0;
 static int test_pass = 0;
 
-#define EXPECT_EQ_BASE(equality, expect, actual, format) \
-    do {\
-        test_count++;\
-        if (equality)\
-            test_pass++;\
-        else {\
-            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
-            main_ret = 1;\
-        }\
-    } while(0)
+// 反斜线(/)代表该行未结束，会串接下一行。而如果宏里有多过一个语句（statement），就需要用 do { /*...*/ } while(0) 包裹成单个语句，否在可能导致部分语句在作用域之外
+#define EXPECT_EQ_BASE(equality, expect, actual, format)                                                           \
+    do                                                                                                             \
+    {                                                                                                              \
+        test_count++;                                                                                              \
+        if (equality)                                                                                              \
+            test_pass++;                                                                                           \
+                                                                                                                   \
+        else                                                                                                       \
+        {                                                                                                          \
+            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual); \
+            main_ret = 1;                                                                                          \
+        }                                                                                                          \
+    } while (0)
 
 #define EXPECT_EQ_INT(expect, actual) EXPECT_EQ_BASE((expect) == (actual), expect, actual, "%d")
 
-static void test_parse_null() {
+static void test_parse_null()
+{
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_OK, lept_parse(&v, "null"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse_expect_value() {
+static void test_parse_expect_value()
+{
     lept_value v;
 
     v.type = LEPT_FALSE;
@@ -39,9 +58,11 @@ static void test_parse_expect_value() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse_invalid_value() {
+static void test_parse_invalid_value()
+{
     lept_value v;
     v.type = LEPT_FALSE;
+
     EXPECT_EQ_INT(LEPT_PARSE_INVALID_VALUE, lept_parse(&v, "nul"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 
@@ -50,21 +71,24 @@ static void test_parse_invalid_value() {
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse_root_not_singular() {
+static void test_parse_root_not_singular()
+{
     lept_value v;
     v.type = LEPT_FALSE;
     EXPECT_EQ_INT(LEPT_PARSE_ROOT_NOT_SINGULAR, lept_parse(&v, "null x"));
     EXPECT_EQ_INT(LEPT_NULL, lept_get_type(&v));
 }
 
-static void test_parse() {
+static void test_parse()
+{
     test_parse_null();
     test_parse_expect_value();
     test_parse_invalid_value();
     test_parse_root_not_singular();
 }
 
-int main() {
+int main()
+{
     test_parse();
     printf("%d/%d (%3.2f%%) passed\n", test_pass, test_count, test_pass * 100.0 / test_count);
     return main_ret;
